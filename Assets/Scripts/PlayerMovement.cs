1dd03f55e58e796dc20478086908621ca9f933d4
erjaman1 <erjamanninen1@kamk.fi>
2024-11-08 21:06:32 +0200

    player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -23,9 +23,25 @@ public class PlayerMovement : MonoBehaviour
         rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate() {
+        MovePlayer();
+    }
+
+    private void Update() {
+        MyInput();
+    }
+
+    private void MyInput() {
+
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    
+    }
+
+    private void MovePlayer() { 
+    
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
     }
 }
