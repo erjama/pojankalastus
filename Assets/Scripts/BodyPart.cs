f6fce188548141d7cda9e657080f4bef5025591a
erjaman1 <erjamanninen1@kamk.fi>
2024-11-09 14:09:43 +0200

    collect bodypart

--- Assets/Scripts/BodyPart.cs
+++ Assets/Scripts/BodyPart.cs
@@ -4,23 +4,15 @@ using UnityEngine.EventSystems;
 public class BodyPart : MonoBehaviour
 {
 
-    [SerializeField] float moveSpeed = 0.1f;
-    Rigidbody rb;
-    Vector3 moveDirection;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+    [SerializeField] private GameObject point;
+    private void OnTriggerEnter(Collider other) {
 
+        if (other.gameObject.name == "harava") {
+
+            this.transform.SetParent(point.gameObject.transform);
+            this.GetComponent<BallScript>().enabled = false;
+            
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        moveDirection = transform.forward;
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
-    }
-
-    //todo despawn bodypart when it goes past the island
 }
