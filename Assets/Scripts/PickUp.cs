9e3746367c971d07ed0833f1de4d96fcdf79557e
erjaman1 <erjamanninen1@kamk.fi>
2024-11-10 15:09:05 +0200

    rake animation

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class PickUp : MonoBehaviour
@@ -7,19 +8,27 @@ public class PickUp : MonoBehaviour
     private bool hasItem = false;
 
     [SerializeField] AudioClip audioClipCollectItem;
+    [SerializeField] private Animator animator;
 
     private void OnTriggerEnter(Collider other) {
 
         if (other.gameObject.tag == "bodyPart" && hasItem == false) {
 
-            hasItem = true;
-            other.gameObject.transform.SetParent(point.gameObject.transform);
-            other.gameObject.transform.localPosition = Vector3.zero;
-            other.gameObject.GetComponent<BallScript>().enabled = false;
-            var rb= other.gameObject.GetComponent<Rigidbody>();
-            Destroy(rb);
-            AudioManager.instance.Play(audioClipCollectItem);
+            animator.SetBool("pickUp", true);
+            StartCoroutine(waitPickUpAnimation(other));
         }
+    }
+
+    IEnumerator waitPickUpAnimation(Collider other) {
+        yield return new WaitForSeconds(1.0f);
+        hasItem = true;
+        other.gameObject.transform.SetParent(point.gameObject.transform);
+        other.gameObject.transform.localPosition = Vector3.zero;
+        other.gameObject.GetComponent<BallScript>().enabled = false;
+        var rb = other.gameObject.GetComponent<Rigidbody>();
+        Destroy(rb);
+        AudioManager.instance.Play(audioClipCollectItem);
+        animator.SetBool("pickUp", false);
     }
 
     public void removeItem() {
