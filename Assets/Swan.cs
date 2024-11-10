5ae583f997bf16e98b8f55d45b12a6d8e8b29431
erjaman1 <erjamanninen1@kamk.fi>
2024-11-10 08:47:21 +0200

    turn bodyparts

--- Assets/Swan.cs
+++ Assets/Swan.cs
@@ -6,7 +6,7 @@ public class Swan : MonoBehaviour
 {
     public Transform[] waypoints;     // Array of waypoints for patrol
     public float patrolSpeed = 2f;    // Speed of enemy when patrolling
-    public float attackRange = 5f;    // Distance at which enemy starts attacking
+    public float attackRange = 10f;    // Distance at which enemy starts attacking
     public float attackSpeed = 3.5f;  // Speed of enemy when attacking
     public float attackCooldown = 2f; // Cooldown between attacks
 
@@ -18,7 +18,6 @@ public class Swan : MonoBehaviour
 
     void Start() {
         navAgent = GetComponent<NavMeshAgent>();
-        //player = GameObject.FindGameObjectWithTag("Player").transform;
         navAgent.speed = patrolSpeed;
     }
 
@@ -26,12 +25,7 @@ public class Swan : MonoBehaviour
 
 
         float distanceToPlayer = Vector3.Distance(transform.position, mummo.position);
-        Vector3 targetDirection = waypoints[currentWaypointIndex].position - transform.position;
-        if (targetDirection != Vector3.zero) {
-            Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 1.0f);
-        }
-
+  
         if (distanceToPlayer <= attackRange) {
             // If player is close, attack
             AttackPlayer();
@@ -50,7 +44,6 @@ public class Swan : MonoBehaviour
     }
 
     void AttackPlayer() {
-        Debug.Log("attack");
         if (Time.time >= nextAttackTime) {
             navAgent.speed = attackSpeed;
             navAgent.SetDestination(mummo.position);
